7f83853e207212d65db43a8982da871e0a63d0e7
Andrew Heo <andrewheo1225@gmail.com>
2020-10-11 20:13:45 -0400

    Style changes

--- cogs/weathercog.py
+++ cogs/weathercog.py
@@ -2,29 +2,30 @@ from discord.ext import commands
 import requests
 from .utils import get_config
 
+
 class WeatherCog(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot # This line in the constructor is required
-    
+        self.bot = bot  # This line in the constructor is required
+
     @commands.command()
-    async def test(self, ctx): # the ctx parameter is short for 'context'
+    async def test(self, ctx):  # the ctx parameter is short for 'context'
         """Sends a message that indicates that the command has worked"""
         await ctx.send("Test succeeded. Command is operational")
 
-
     @commands.command()
     async def weather(self, ctx):
-        config = get_config()    
+        config = get_config()
         payload = {
-            "q" : "New Brunswick,US-NJ,US",
-            "appid" : config["WeatherAPIKey"],
-         "units" : "imperial"
+            "q": "New Brunswick,US-NJ,US",
+            "appid": config["WeatherAPIKey"],
+            "units": "imperial",
         }
-        response = requests.get("http://api.openweathermap.org/data/2.5/weather", params=payload)       
+        response = requests.get(
+            "http://api.openweathermap.org/data/2.5/weather", params=payload
+        )
         response_content = response.json()
         temp = response_content["main"]["temp"]
         await ctx.send(f"The weather in New Brunswick is {temp}")
-
 
 
 def setup(bot):
